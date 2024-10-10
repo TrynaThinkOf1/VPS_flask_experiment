30f946bd92ebe34901bb4e79221c68081077575c
Zevi Berlin <zevi@sandbocks>
2024-10-10 21:41:42 +0000

    add a route for asking what the last button was

--- hello.py
+++ hello.py
@@ -3,14 +3,25 @@ import time as t
 
 app = Flask(__name__)
 
+lastButtonPress = 0
+
 @app.route("/")
 def hello_world():
     return render_template('index.html')
 
 @app.route('/but<int:button_id>', methods=['GET'])
 def button_clicked(button_id):
+    global lastButtonPress # not allowed to modify a global variable in a function unless you do this
     print(f"Button {button_id} Clicked!")
+    lastButtonPress = button_id # update the info of what button was pressed last
     return render_template('index.html')
+
+@app.route("/lastbut")
+def lastbut():
+    global lastButtonPress # not allowed to modify a global variable in a function unless you do this
+    temp = lastButtonPress
+    lastButtonPress = 0 # reset the thing when the info is handed over
+    return str(temp) # must return a string not a number, this is web stuff
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000, debug=True) #this is so we dont have to do the specialized initiation command
