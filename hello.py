62aa3362f84c6a67ff3c315143849bf3f2bf5131
Zevi Berlin <zevi@sandbocks>
2024-10-11 04:04:52 +0000

    remove comments and stuff

--- hello.py
+++ hello.py
@@ -11,22 +11,17 @@ def hello_world():
 
 @app.route('/but<int:button_id>', methods=['GET'])
 def button_clicked(button_id):
-    global lastButtonPress # not allowed to modify a global variable in a function unless you do this
+    global lastButtonPress
     print(f"Button {button_id} Clicked!")
     lastButtonPress = button_id # update the info of what button was pressed last
     return render_template('index.html')
 
 @app.route("/lastbut")
 def lastbut():
-    global lastButtonPress # not allowed to modify a global variable in a function unless you do this
+    global lastButtonPress
     temp = lastButtonPress
-    lastButtonPress = 0 # reset the thing when the info is handed over
-    return str(temp) # must return a string not a number, this is web stuff
+    lastButtonPress = 0
+    return str(temp)
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000, debug=True) #this is so we dont have to do the specialized initiation command
-
-#yea after ill the python function syntax i read to achieve these beautiful functions, i have no idea
-#whats going wrong
-
-
+    app.run(host="0.0.0.0", port=5000, debug=True)
