e9088ab064dab1cc60d0e9fc4d372dd060feb346
Zevi Berlin <zevi@sandbocks>
2024-10-12 18:09:03 +0000

    add an endpoint for sending text from outside, and for retrieving it

--- hello.py
+++ hello.py
@@ -1,23 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, request
 import time as t
 
 app = Flask(__name__)
 
 lastButtonPress = 0
-rec_text = ""
+rec_text = "bla"
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/but<int:button_id>', methods=['GET'])
 def button_clicked(button_id):
-    global lastButtonPress
-    print(f"Button {button_id} Clicked!")
-    lastButtonPress = button_id
-    return render_template('index.html')
+	global lastButtonPress # not allowed to modify a global variable in a function unless you do this
+	print(f"Button {button_id} Clicked!")
+	lastButtonPress = button_id # update the info of what button was pressed last
+	return render_template('index.html')
 
-# Updated route to handle text input via GET request
 @app.route('/print_text', methods=['GET'])
 def print_text():
     global rec_text
@@ -26,19 +25,22 @@ def print_text():
     print(f"Received text: {text}")
     return render_template('index.html')
 
-@app.route('/text')
+@app.route('/text', methods=['GET'])
 def received_text():
 	global rec_text
-	temp = rec_text
-	rec_text = ""
-	return temp
+	rec_text = request.args.get('sjfldak')
+	return "thanks"
+
+@app.route('/seetext')
+def seetext():
+    return rec_text
 
 @app.route("/lastbut")
 def lastbut():
-    global lastButtonPress
-    temp = lastButtonPress
-    lastButtonPress = 0
-    return str(temp)
+	global lastButtonPress # not allowed to modify a global variable in a function unless you do this
+	temp = lastButtonPress
+	lastButtonPress = 0 # reset the thing when the info is handed over
+	return str(temp) # must return a string not a number, this is web stuff
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+	 app.run(host="0.0.0.0", port=5000, debug=False) #this is so we dont have to do the specialized initiation command
