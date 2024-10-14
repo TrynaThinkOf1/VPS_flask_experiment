713bdc91899f0a7e81fb2f72823260c50bed56b7
Zevi Berlin <zevi@sandbocks>
2024-10-14 21:40:37 +0000

    lots of improvement

--- hello.py
+++ hello.py
@@ -1,46 +1,60 @@
-from flask import Flask, render_template, jsonify, request
-import time as t
+from flask import Flask, render_template, jsonify, request, redirect, send_file
+import os
 
 app = Flask(__name__)
 
 lastButtonPress = 0
-rec_text = "bla"
+rec_text = ""
+output = ""
+
+total_output = []
+
+#def load_image():
+#	img_dir = "static"
+#	img_list = os.listdir(img_dir)
+#	img_path = os.path.join(img_dir, str(img_list))
+#	return img_path
 
 @app.route("/")
-def hello_world():
+def main():
 	return render_template('index.html')
-
-@app.route('/but<int:button_id>', methods=['GET'])
-def button_clicked(button_id):
-	global lastButtonPress # not allowed to modify a global variable in a function unless you do this
-	print(f"Button {button_id} Clicked!")
-	lastButtonPress = button_id # update the info of what button was pressed last
-	return render_template('index.html')
-
-@app.route('/print_text', methods=['GET'])
-def print_text():
-    global rec_text
-    text = request.args.get('input')  # Get the 'input' parameter from the URL
-    rec_text = text
-    print(f"Received text: {text}")
-    return render_template('index.html')
 
 @app.route('/text', methods=['GET'])
 def received_text():
 	global rec_text
-	rec_text = request.args.get('sjfldak')
-	return "thanks"
+	rec_text = request.args.get('command')
+	return redirect("/", code=302)
 
-@app.route('/seetext')
+@app.route('/see_text')
 def seetext():
-    return rec_text
+	global rec_text
+	temp = rec_text
+	rec_text = ""
+	return str(temp)
 
-@app.route("/lastbut")
-def lastbut():
-	global lastButtonPress # not allowed to modify a global variable in a function unless you do this
-	temp = lastButtonPress
-	lastButtonPress = 0 # reset the thing when the info is handed over
-	return str(temp) # must return a string not a number, this is web stuff
+@app.route('/output_text', methods=['GET'])
+def output():
+	global output
+	global total_output
+	output = request.args.get('output')
+	total_output.append(output)
+	return redirect("/", code=302)
+
+@app.route('/see_output')
+def show_output():
+	return total_output
+
+@app.route('/clear_output')
+def clear_output():
+	global total_output
+	total_output.clear()
+	return redirect("/", code=302)
+
+@app.route('/image_output')
+def show_screenshots():
+#	image = load_image()
+	return send_file("static/screenshot.png", mimetype='image/png')
 
 if __name__ == "__main__":
-	 app.run(host="0.0.0.0", port=5000, debug=False) #this is so we dont have to do the specialized initiation command
+	 app.run(host="0.0.0.0", port=5000, debug=True) #this is so we dont have to do the specialized initiation command
+
